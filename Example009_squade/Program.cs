26d299ce0d10b6efc767a1b56e71f34a9d342de5
Bobby <mif210210@gmail.com>
2021-11-24 02:49:03 +0300

    add Example009_squade

---
+++ Example009_squade/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+double result = num * num;
+Console.Write("Квадрат числа: ");
+Console.WriteLine(result);
+
+
+
+
+
