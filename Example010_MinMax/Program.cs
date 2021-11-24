06db2959d8274e8d07e718fec1c0888cd0497beb
Bobby <mif210210@gmail.com>
2021-11-24 03:04:31 +0300

    add Example010_MinMax

---
+++ Example010_MinMax/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+double numA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numB = Convert.ToDouble(Console.ReadLine());
+
+if(numA < numB)
+{
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(numA);
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numB);
+}
+else
+{
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(numB);
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numA);
+}
