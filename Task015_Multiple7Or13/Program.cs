84212e483fca9b3b39a67e1887df7dc3af5bc026
Bobby <mif210210@gmail.com>
2021-11-27 02:38:29 +0300

    update

--- Task015_Multiple7Or13/Program.cs
+++ Task015_Multiple7Or13/Program.cs
@@ -1,16 +1,10 @@
 ﻿// Выяснить кратно ли число 7
 Console.WriteLine("Введи число: ");
 double num = Convert.ToDouble(Console.ReadLine());
-if (num % 7 == 0)
+if (num % 7 == 0 & num % 23 == 0)
 {
-    if (num % 23 == 0)
-    {
-        Console.WriteLine("Кратно");
-    }
-    else
-    {
-        Console.WriteLine("Не кратно");
-    }
+
+    Console.WriteLine("Кратно");
 }
 else
 {
