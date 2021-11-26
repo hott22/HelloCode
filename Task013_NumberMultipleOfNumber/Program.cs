cf3e719eb399efc157a6a001b14b4dc8ddf45241
Bobby <mif210210@gmail.com>
2021-11-27 01:40:54 +0300

    add Task013_NumberMultipleOfNumber

---
+++ Task013_NumberMultipleOfNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Кратно ли число заданному? если нет, вывести остаток.
+
+Console.WriteLine("Введи число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Кратно чему: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    double num3 = num1 % num2;
+    Console.Write("Не кратно, остаток: ");
+    Console.WriteLine(num3);
+}
