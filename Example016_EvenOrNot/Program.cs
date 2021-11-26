f61305c86254e8f72b63da61a818afd2144c9a99
Bobby <mif210210@gmail.com>
2021-11-27 00:24:30 +0300

    add Example016_EvenOrNot

---
+++ Example016_EvenOrNot/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введи число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+if (num % 2 == 1)
+{
+    Console.WriteLine("Нечетное");
+}
+else
+{
+    Console.WriteLine("Четное");
+}
