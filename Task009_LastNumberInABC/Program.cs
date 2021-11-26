cdc6c042fdd023468d9fa4fd4d59a644a3a01b66
Bobby <mif210210@gmail.com>
2021-11-27 02:38:54 +0300

    add Task009_LastNumberInABC

---
+++ Task009_LastNumberInABC/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введи трехзначное число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+if (num1 > 999 | num1 < 100)
+{
+    Console.WriteLine("Не трехзначное");
+}
+else
+{
+    double num2 = num1 % 100;
+    double num3 = num2 % 10;
+    Console.Write("Последняя цифра: ");
+    Console.WriteLine(num3);
+}
+
