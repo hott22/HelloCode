a89946c818b76a38afe4edeeb2586a600497b4f0
Bobby <mif210210@gmail.com>
2021-11-27 03:14:59 +0300

    add Task012_DelSecondNumberABC

---
+++ Task012_DelSecondNumberABC/Program.cs
@@ -0,0 +1,17 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введи трехзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 > 999 | num1 < 100)
+{
+    Console.WriteLine("Не трехзначное");
+}
+else
+{
+    int num2 = num1 % 100;
+    int num3 = num2 % 10;
+    int num4 = num1 / 100;
+    int num5 = num4*10 + num3;
+    Console.Write("Без второй цифры: ");
+    Console.WriteLine(num5);
+}
+
