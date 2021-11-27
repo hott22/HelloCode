e229a5f51dc19a31fc1c5fab654dfd1cc4080c7b
Bobby <mif210210@gmail.com>
2021-11-27 10:28:18 +0300

    add Task005_FunctionF

---
+++ Task005_FunctionF/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+Console.WriteLine("Введи a");
+double a = Convert.ToDouble(Console.ReadLine());
+double function(double a)
+{
+    double f = a * a + 23;
+    return f;
+}
+Console.WriteLine(function(a));
