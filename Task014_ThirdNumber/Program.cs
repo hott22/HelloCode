8edc7a2c5db8c115779fb5b3ec6f45ac1f32bf99
Bobby <mif210210@gmail.com>
2021-11-27 03:34:12 +0300

    add Task014_ThirdNumber

---
+++ Task014_ThirdNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введи число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 < 100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    int num2 = num1 / 100;
+    int num3 = num2 % 10;
+    Console.Write("Третья цифра: ");
+    Console.WriteLine(num3);
+}
