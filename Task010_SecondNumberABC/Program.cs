50723c4e23efbe241715f4d2758fe415d4c490bc
Bobby <mif210210@gmail.com>
2021-11-27 03:14:08 +0300

    update Task010_SecondNumberABC

--- Task010_SecondNumberABC/Program.cs
+++ Task010_SecondNumberABC/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Показать вторую цифру трёхзначного числа
 Console.WriteLine("Введи трехзначное число: ");
-double num1 = Convert.ToDouble(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 if (num1 > 999 | num1 < 100)
 {
     Console.WriteLine("Не трехзначное");
 }
 else
 {
-    double num2 = num1 % 100;
-    double num3 = num2 % 10;
-    double num4 = (num2 - num3) / 10;
-    Console.Write("Вторая ицфра: ");
+    int num2 = num1 % 100;
+    int num3 = num2 % 10;
+    int num4 = (num2 - num3) / 10;
+    Console.Write("Вторая цифра: ");
     Console.WriteLine(num4);
 }
