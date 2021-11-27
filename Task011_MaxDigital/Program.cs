f2a8def279bc3c2c6f48ee94ec59d7fd034faada
Bobby <mif210210@gmail.com>
2021-11-27 12:31:24 +0300

    add Task011_MaxDigital

---
+++ Task011_MaxDigital/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int num = new Random().Next(10, 99);
+Console.Write("Рандомное число: ");
+Console.WriteLine(num);
+int digital1 = num % 10;
+int digital2 = num / 10;
+if (digital1 > digital2)
+{
+    Console.Write("Наибольшая цифра ");
+    Console.Write(digital1);
+}
+else
+{
+    Console.Write("Наибольшая цифра ");
+    Console.Write(digital2);
+}
