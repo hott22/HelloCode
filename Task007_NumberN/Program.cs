192d2e178db64a2e622cd312a1efbbc627a87f1d
Bobby <mif210210@gmail.com>
2021-11-28 10:40:59 +0300

    add Task007_NumberN

---
+++ Task007_NumberN/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Введи N");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+while (count < N + 1)
+{
+
+    Console.WriteLine(count);
+    count = count + 1;
+
+
+}
+
